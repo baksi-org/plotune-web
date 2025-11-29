8f83dd70ce59dfab075ca328b3be982c1bd7bb67
kantrveysel <kantrveysel@gmail.com>
2025-11-29 17:07:26 +0300

    fix ai bullshit again

--- src/services/api.js
+++ src/services/api.js
@@ -41,29 +41,4 @@ api.interceptors.response.use(
   }
 );
 
-// Same for stream API
-streamApi.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('auth_token');
-    if (token) {
-      config.headers.Authorization = `${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-streamApi.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      Cookies.remove('auth_token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default api;
